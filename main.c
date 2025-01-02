fa3e97b0541f315a01414d64eea4cafdff02d50b
Jenya Knyazev <davidknyazev409@gmail.com>
2025-01-02 10:42:30 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int digits,exercises,rounds,sum,sum2,correct,wrong,pm;
+#define GREEN FOREGROUND_GREEN
+#define RED FOREGROUND_RED
+#define BLUE FOREGROUND_BLUE
+#define WHITE BLUE|RED|GREEN
+int digits,exercises,rounds,sum,sum2,correct,wrong,pm,delay;
 HANDLE h;
 COORD c;
 clock_t start,end;
+int colors[] = {GREEN,RED,BLUE,RED|GREEN,RED|BLUE,BLUE|GREEN};
 void clean(){
     SetConsoleCursorPosition(h,c);
     puts("                                                                        ");
     SetConsoleCursorPosition(h,c);
 }
-
+int change_num_color(int prev_color) {
+    int color;
+    while ((color = colors[rand() % 6]) == prev_color);
+    SetConsoleTextAttribute(h, color);
+    return color;
+}
 void input(){
     do{
         printf("enter 1 for simple, enter 2 for interesting: ");
@@ -43,25 +54,32 @@ int gen_num(){
         return n*-1;
     return n;
 }
-void jump_num(){
-    int n=rand()%30,k=gen_num();
+int jump_num(int color,int *prev_n){
+    int n,k=gen_num();
+    while ((n = rand() % 40) >= (*prev_n)-digits&& n <= (*prev_n)+digits);
+    (*prev_n) = n;
     while(n--){
         putchar(' ');
     }
     sum+=k;
+    color=change_num_color(color);
     printf("%d",k);
-    Sleep(1000);
+    Sleep(delay);
     clean();
+    return color;
 }
 void round_(){
     int e=exercises;
+    int color = -1;
+    int prev_jump_location=-1;
     sum=0;
     printf("Ready press any key");
     getchar();
     clean();
     while(e--){
-        jump_num();
+        color=jump_num(color,&prev_jump_location);
     }
+    SetConsoleTextAttribute(h, WHITE);
     printf("enter the sum: ");
     scanf("%d",&sum2);
     getchar();
@@ -78,6 +96,10 @@ void round_(){
 }
 void play(){
     int again,t;
+    printf("Enter delay in miliseconds: ");
+    scanf("%d", &delay);
+    getchar();
+    clean();
     do{
         input();
         start=clock()/CLOCKS_PER_SEC;
@@ -89,7 +111,7 @@ void play(){
         t=end-start;
         printf("correct: %d, wrong: %d, total: %d,time %d:%d",correct,wrong
         ,correct+wrong,t/60,t%60);
-        Sleep(5000);
+        Sleep(10000);
         clean();
         printf("again enter 0 other exit: ");
         scanf("%d",&again);
@@ -97,7 +119,7 @@ void play(){
         clean();
     }while(!again);
     printf("Good Bay");
-    Sleep(5000);
+    Sleep(10000);
 }
 int main(){
     srand(time(NULL));
